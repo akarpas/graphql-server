c1ff6ff816e762d0543414142d9d60b092ef937c
Andreas Karpasitis <andreas.karpasitis@utopiamusic.com>
2018-07-05 12:46:37 +0200

    setup sample data

---
+++ data/moviesData.js
@@ -0,0 +1,32 @@
+const moviesData = [
+  {
+    id: 1,
+    title: 'Pulp Fiction',
+    director: 'Quentin Tarantino',
+    synopsis: 'Four different stories of four different people and how they connect together',
+    genre: 'Action' 
+  },
+  {
+    id: 2,
+    title: 'Minority Report',
+    director: 'Steven Spielberg',
+    synopsis: 'In the future crimes are predicted and stopped before they take place. But is the system flaut?',
+    genre: 'Sci-Fi' 
+  },
+  {
+    id: 3,
+    title: 'Kill Bill',
+    director: 'Quentin Tarantino',
+    synopsis: 'A woman assassin has been left for dead, now she is an a mission for revenge; to kill Bill',
+    genre: 'Action' 
+  },
+  {
+    id: 4,
+    title: 'The Hangover',
+    director: 'Todd Phillips',
+    synopsis: 'Four friends embark on a simple bachelor party in Las Vegas, until everything gets out of control',
+    genre: 'Comedy' 
+  }
+]
+
+module.exports = moviesData
